90ed5d80adc599809dae6eadc2dbf185e8952eba
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-01-11 17:12:39 +0330

    Homework one is completed

--- Folder_1_(2022-01-11)/JS-2-1-first-homework.js
+++ Folder_1_(2022-01-11)/JS-2-1-first-homework.js
@@ -4,6 +4,7 @@ They are essential practice for the rest of the homework tasks.
 If you get stuck, reach out to your classmates on Telegram for help!
 */
 
+
 // !#25-quiz8.js
 /*
 1- Define an array containing the 3 persons defined below.
@@ -65,4 +66,178 @@ console.log(
     : "Not yet :("
 );
 
+
 // !#26-quiz9.js
+/*
+An array of travel destinations is defined below.Each destination has a name, a distance from Glasgow, and a list of transportations available to go there.
+
+1- Filter the travelDestinations array to return all destination names reachable within 500 kms.
+2- Find a destination name reachable by ferry.
+3- Print in the console all the destination names more than 300 kms far away and reachable by train.
+*/
+
+const destination1 = {
+  destinationName: "Edinburgh",
+  distanceKms: 80,
+  transportations: ["car", "bus", "train"],
+};
+
+const destination2 = {
+  destinationName: "London",
+  distanceKms: 650,
+  transportations: ["car", "bus", "train"],
+};
+
+const destination3 = {
+  destinationName: "Paris",
+  distanceKms: 900,
+  transportations: ["train", "plane"],
+};
+
+const destination4 = {
+  destinationName: "Dublin",
+  distanceKms: 350,
+  transportations: ["plane", "ferry"],
+};
+
+const travelDestinations = [
+  destination1,
+  destination2,
+  destination3,
+  destination4,
+];
+
+/*
+DO NOT EDIT ANYTHING ABOVE THIS LINE
+WRITE YOUR CODE BELOW
+*/
+
+const destinationNamesWithin500Kms = travelDestinations
+  .filter((item) => item.distanceKms < 500
+  ).map((item) => item.destinationName);// Complete here
+
+const destinationNameReachableByFerry = travelDestinations.filter((item) =>
+  item.transportations.some((element) => element === "ferry") ? item : false
+).map(item=>item.destinationName);// Complete here
+
+const destinationNamesMoreThan300KmsAwayByTrain = travelDestinations
+  .filter((item) =>
+    item.transportations.some((element) => element === "train") && item.distanceKms > 300 ? item : false
+  ).map((item) => item.destinationName); // Complete here (PRINT THE RESULT IN THE CONSOLE USING FOREACH)
+
+/*
+DO NOT EDIT ANYTHING BELOW THIS LINE
+*/
+
+console.log(
+  `Question 1) Expected result: Edinburgh,Dublin, actual result: ${destinationNamesWithin500Kms}`
+);
+console.log(
+  `Question 2) Expected result: Dublin, actual result: ${destinationNameReachableByFerry}`
+);
+console.log(
+  `Question 3) Expected result: London,Paris, actual result: ${destinationNamesMoreThan300KmsAwayByTrain}`
+);
+
+
+// !#27-quiz9.js
+/*
+Below are some restaurants in Glasgow. Each restaurant has a name, the number of total seats, the number of customers in the restaurant at the moment and the menu with prices.
+
+We want to build an object to simulate a Restaurant Finder application (see below restaurantFinderApplication).
+
+1- Define a method findAvailableRestaurants which takes a number of people in parameter and returns all the restaurant names which have the required number of seats available at the moment.
+
+2- Define a method findRestaurantServingDish which takes a dish name in parameter and returns all the restaurant names serving this dish.
+
+3- Define a method countNumberOfRestaurantsInArea which takes an area of Glasgow in parameter (center, west), and returns the number of restaurants in this area.
+*/
+
+const restaurant1 = {
+  name: "Paesano",
+  totalSeats: 10,
+  numberOfCustomers: 8,
+  address: {
+    city: "Glasgow",
+    area: "center",
+  },
+  menu: ["pizza", "calzone", "salad"],
+};
+
+const restaurant2 = {
+  name: "Ubiquitous Chip",
+  totalSeats: 20,
+  numberOfCustomers: 10,
+  address: {
+    city: "Glasgow",
+    area: "west",
+  },
+  menu: ["salad", "chocolate cake", "roast lamb"],
+};
+
+const restaurant3 = {
+  name: "Monkeyz",
+  totalSeats: 15,
+  numberOfCustomers: 8,
+  address: {
+    city: "Glasgow",
+    area: "center",
+  },
+  menu: ["stew", "chocolate cake", "panini"],
+};
+
+const restaurants = [restaurant1, restaurant2, restaurant3];
+
+/*
+DO NOT EDIT ANYTHING ABOVE THIS LINE
+WRITE YOUR CODE BELOW
+*/
+
+const restaurantFinderApplication = {
+  applicationName: "Restaurant Finder",
+  applicationVersion: "1.0",
+  restaurants: restaurants,
+  findAvailableRestaurants: function (numberOfPeople) {
+    // Complete here
+    return this.restaurants.filter(
+      (item) => item.totalSeats - item.numberOfCustomers >= numberOfPeople
+    ).map(element=>element.name);
+  },
+  findRestaurantServingDish: function (dishName) {
+    // Complete here
+    return this.restaurants
+      .filter((item) => item.menu.some((ischeck) => ischeck === dishName))
+      .map((element) => element.name);
+  },
+  countNumberOfRestaurantsInArea: function (area) {
+    // Complete here
+    return this.restaurants
+      .filter((item) => item.address.area === area)
+      .map((element) => element.name);
+  },
+};
+
+/*
+DO NOT EDIT ANYTHING BELOW THIS LINE
+*/
+
+let restaurantsAvailableFor5People = restaurantFinderApplication.findAvailableRestaurants(
+  5
+)
+console.log(
+  `Find available restaurants for 5 people: Expected result: Ubiquitous Chip,Monkeyz, actual result: ${restaurantsAvailableFor5People}`
+)
+
+let restaurantsServingSalad = restaurantFinderApplication.findRestaurantServingDish(
+  "salad"
+)
+console.log(
+  `Find restaurants serving salad: Expected result: Paesano,Ubiquitous Chip, actual result: ${restaurantsServingSalad}`
+)
+
+let numberOfRestaurantsInCityCentre = restaurantFinderApplication.countNumberOfRestaurantsInArea(
+  "center"
+)
+console.log(
+  `Number of restaurants in city centre: Expected result: 2, actual result: ${numberOfRestaurantsInCityCentre}`
+)
