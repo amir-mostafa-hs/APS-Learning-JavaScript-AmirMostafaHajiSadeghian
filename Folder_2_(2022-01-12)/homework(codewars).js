0efad1ba6c528dcf95b3716adf028764e201f1d0
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-01-13 02:22:49 +0330

    completing Crash-Override-Kata in codewars

--- Folder_2_(2022-01-12)/homework(codewars).js
+++ Folder_2_(2022-01-12)/homework(codewars).js
@@ -1,4 +1,4 @@
-// #1 
+// !#1 
 /*
 URL: https://www.codewars.com/kata/56c22c5ae8b139416c00175d/train/javascript
 
@@ -27,3 +27,44 @@ function match(candidate, job) {
     } 
 }
 
+// !#2
+/*
+ULR: https://www.codewars.com/kata/crash-override/train/javascript
+
+Every budding hacker needs an alias! The Phantom Phreak, Acid Burn, Zero Cool and Crash Override are some notable examples from the film Hackers.
+
+Your task is to create a function that, given a proper first and last name, will return the correct alias.
+
+Two objects that return a one word name in response to the first letter of the first name and one for the first letter of the surname are already given.
+
+If the first character of either of the names given to the function is not a letter from A - Z, you should return "Your name must start with a letter from A - Z."
+
+Sometimes people might forget to capitalize the first letter of their name so your function should accommodate for these grammatical errors.
+
+var firstName = {A: 'Alpha', B: 'Beta', C: 'Cache' ...}
+var surname = {A: 'Analogue', B: 'Bomb', C: 'Catalyst' ...}
+
+aliasGen('Larry', 'Brentwood') === 'Logic Bomb'
+aliasGen('123abc', 'Petrovic') === 'Your name must start with a letter from A - Z.'
+*/
+
+function aliasGen(firstNoun, lastNoun) {
+  const firstCharOfFirstNoun = firstNoun[0].toUpperCase();
+  const firstCharOfLastNoun = lastNoun[0].toUpperCase();
+  if (
+    firstName[firstCharOfFirstNoun] === undefined ||
+    surname[firstCharOfLastNoun] === undefined
+  ) {
+    return "Your name must start with a letter from A - Z.";
+  } else {
+    return firstName[firstCharOfFirstNoun] + " " + surname[firstCharOfLastNoun];
+  }
+}
+
+
+/**
+ * amalkard emroz ar sat 6pm ta 9pm class jobrani mahya va 
+ * az sat 10pm ta 2 am hal kardan katahaye bala 😐😑
+ * ** AZ BAZI AZ KATA HAY CODE WARS MOTENAFERAM **
+ * 
+ */
