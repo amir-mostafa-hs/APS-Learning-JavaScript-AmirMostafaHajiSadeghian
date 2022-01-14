fa6bd15ad9e06fbbd01acc7b42dc19575752b27f
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-01-14 08:06:54 +0330

    codewars exercise 5

--- Folder_4_(2022-01-14)/Daily-work-on-the-fourth-day.js
+++ Folder_4_(2022-01-14)/Daily-work-on-the-fourth-day.js
@@ -71,3 +71,41 @@ function animal(obj) {
   return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
 }
 
+// !#5(Split The Bill)
+/*
+ULR: https://www.codewars.com/kata/5641275f07335295f10000d0/train/javascript
+
+It's tricky keeping track of who is owed what when spending money in a group. Write a function to balance the books.
+
+The function should take one parameter: 
+  an object/dict with two or more name-value pairs which represent the 
+  members of the group and the amount spent by each.
+
+The function should return an object/dict with the same names, 
+showing how much money the members should pay or receive.
+
+Further points:
+  The values should be positive numbers if the person should receive money from the group,
+  negative numbers if they owe money to the group.
+
+  If value is a decimal, round to two decimal places.
+
+Example:
+  3 friends go out together: A spends £20, B spends £15, and C spends £10. 
+  The function should return an object/dict showing that A should receive £5, 
+  B should receive £0, and C should pay £5.
+*/
+
+function splitTheBill(obj) {
+  const arrayForAverage =
+    Object.values(obj).reduce((num1, num2) => num1 + num2) /
+    Object.values(obj).length;
+  const result = {};
+  for (const key in obj) {
+    let number = obj[key] - arrayForAverage;
+    result[key] = Number.isInteger(number)
+      ? number
+      : parseFloat(number.toFixed(2));
+  }
+  return result;
+}
