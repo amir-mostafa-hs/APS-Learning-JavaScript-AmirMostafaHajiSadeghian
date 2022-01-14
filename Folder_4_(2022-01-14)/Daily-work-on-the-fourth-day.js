e944d6008beda09a6ba31e3b44e2ca5a8af48982
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-01-14 07:13:35 +0330

    codewars exercise 4

--- Folder_4_(2022-01-14)/Daily-work-on-the-fourth-day.js
+++ Folder_4_(2022-01-14)/Daily-work-on-the-fourth-day.js
@@ -19,7 +19,7 @@ Possible invalid inputs include:
   IP_ADDRESS_NOT_FOUND - ip address not in the database
   IP_ADDRESS_REQUIRED - no ip address was supplied
 */
-// ?
+
 const greetData = {
   english: "Welcome",
   czech: "Vitejte",
@@ -44,5 +44,30 @@ function greet(language) {
   return greetData[language] !== undefined
     ? greetData[language]
     : greetData["english"];
+}
+
+// !#4(Training JS #5: Basic data types--Object)
+/*
+ULR: https://www.codewars.com/kata/571f1eb77e8954a812000837/train/javascript
+
+In javascript, Object is one of basic data types. Define an Object can use var obj=new Object() or var obj={}
+
+You can define the object attributes during initialization, like this:
+  var animal={name:"dog"}
+
+you can also set/get some properties after the object definition, like this:
+  var animal={}
+  animal.name="dog"  (or animal["name"]="dog")
+
+Task
+Give you a function animal, accept 1 parameter:obj like this:
+  {name:"dog",legs:4,color:"white"}
+
+and return a string like this:
+  "This white dog has 4 legs."
+*/
+
+function animal(obj) {
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
 }
 
