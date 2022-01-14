ea8704cdeef0f2b78d360e2d5f4622167c16ffd3
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-01-14 21:15:09 +0330

    reviewing JS1-1 session (String Section)

---
+++ Folder_4_(2022-01-14)/Reinforcement-class-for-JS1-1.js
@@ -0,0 +1,39 @@
+/*
+ * reviewing JS1-1 session after class
+ 
+ * tafavot Number() va parseInt()
+  ? Number("123d") => Nan  and parseInt("123d") => 123
+
+ * variable type let => good type
+                ? const => good type and they need initializing value
+                   ! in const we can't use ++ or -- and change value
+                ? var => not good type and don't use
+                ? boolean => its good  and boolean value is true or false 
+
+ * in variables ,we use camelCase mode to specify the name
+
+ * Declaring a string variable =>  let str = "testStr" //! let str = "" this is the name empty string
+ * Declaring a number variable =>  let num = 123
+ * Declaring a boolean variable =>  let isBool = true
+ 
+ * math operations => +,-,*,**,/,%,NaN
+
+ * addition assignment operator => ++ , -- , += , -= , *= , ...
+   ? example: let a = 10; a++ or ++a 
+
+ ! let num = 10 { log(num++) => 10
+              ! { log(++num) => 11
+
+ * String Section:
+            ? 1-in the string, the index starts from 0.
+            ? 2-to access the character we can use [] after the variable name.
+            ? 3-to see the how many characters includes of string we can use (.length) after the variable name.
+            ? 4-We can concatenate the strings by (+) or (.concat)
+            ? 5-if we add numbers to strings, we will receive string finally
+      !example:
+               1- let str = "strTest" => s t r T e s t
+                                         0 1 2 3 4 5 6
+
+               2- let str = "strTest" , str[3] => "T"
+               
+ */
