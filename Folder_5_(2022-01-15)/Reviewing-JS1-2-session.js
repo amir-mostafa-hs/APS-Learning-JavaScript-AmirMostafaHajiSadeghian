538c882af9772412041d57e8f0eb35f41109ad6e
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-01-15 23:57:39 +0330

    reviewing JS1-2 session (Comparisons section)

---
+++ Folder_5_(2022-01-15)/Reviewing-JS1-2-session.js
@@ -0,0 +1,20 @@
+/*
+ * I participated in the reinforcement class for JS1-2 (6 to 11:30) 
+     (Arad, Khashayar, Mohammad Hossein and Kian class)
+ * 
+ * reviewing JS1-2 session after class : 
+
+
+ * Comparisons:
+    ? 1- (>) greater than 
+    ? 2- (<) less than 
+    ? 3- (>=) greater than or equal to
+    ? 4- (<=) less than or equal to
+    ? 5- (==) equality(only checked value not type) //! don't use it
+    ? 6- (!=) not equal(only checked value not type) //! don't use it
+    ? 7- (===) strict equality(checked value and type)
+    ? 8- (!==) strict inequality or strict non equality(checked value and type)
+
+ * 
+
+ */
