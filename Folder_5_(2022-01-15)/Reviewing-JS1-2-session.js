c86ff9cb11973262c8f8c8740a1449f67094dae4
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-01-16 04:17:04 +0330

    reviewing JS1-2 session (few options to know)

--- Folder_5_(2022-01-15)/Reviewing-JS1-2-session.js
+++ Folder_5_(2022-01-15)/Reviewing-JS1-2-session.js
@@ -15,6 +15,23 @@
     ? 7- (===) strict equality(checked value and type)
     ? 8- (!==) strict inequality or strict non equality(checked value and type)
 
- * 
+ * console.log() =>//? The console.log() method outputs a message to the web console.
+                     ? The message may be a single string (with optional substitution values), 
+                     ? or it may be any one or more JavaScript objects.
 
+ * alert(): => //? alert() instructs the browser to display a dialog with an optional message, 
+                 ? and to wait until the user dismisses the dialog. 
+
+                 ? Under some conditions — for example, when the user switches tabs — the browser may not actually display a dialog, 
+                 ? or may not wait for the user to dismiss the dialog.
+
+ * prompt(): => //? prompt() instructs the browser to display a dialog with an optional message prompting the user to input some text, 
+                  ? and to wait until the user either submits the text or cancels the dialog. 
+                  
+                  ? Under some conditions — for example, when the user switches tabs — the browser may not actually display a dialog, 
+                  ? or may not wait for the user to submit text or to cancel the dialog.
+
+ * parseInt(): => //? The parseInt() function parses a string argument and returns an integer of the specified radix
+
+                  
  */
