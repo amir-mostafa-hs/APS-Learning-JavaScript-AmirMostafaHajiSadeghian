f39d5a8907e1a98d1256e30d08d326fbc67f8fdc
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-01-24 23:36:05 +0330

    default parameters section 3

--- Folder_7_(2022-01-24)/Reviewing-JS1-2-session.js
+++ Folder_7_(2022-01-24)/Reviewing-JS1-2-session.js
@@ -14,16 +14,16 @@
 
     *The default parameters of the ES6 product are in javascript; declaration the default parameters in JavaScript: 
 */
-function testAssignedDefaultParameters(params1 = "one" , params2 = "two") {
-    return (`function parameter value: 1- *${params1}* 2- *${params2}*`);
+function testAssignedDefaultParameters(params1 = "one", params2 = "two") {
+  return `function parameter value: 1- *${params1}* 2- *${params2}*`;
 }
-console.log(testAssignedDefaultParameters()); 
+console.log(testAssignedDefaultParameters());
 // function parameter value: 1- *one* 2- *two*
 
 console.log(testAssignedDefaultParameters("hello"));
 // function parameter value: 1- *hello* 2- *two*
 
-console.log(testAssignedDefaultParameters("hello","amir"));
+console.log(testAssignedDefaultParameters("hello", "amir"));
 // function parameter value: 1- *hello* 2- *amir*
 
 /*
@@ -40,9 +40,9 @@ console.log(testAssignedDefaultParameters(undefined));
     Therefore, a new object is created each time the function is called.
 */
 
-function evaluationDefaultArguments(params , arrayParams = []) {
-    arrayParams.push(params);
-    return arrayParams;
+function evaluationDefaultArguments(params, arrayParams = []) {
+  arrayParams.push(params);
+  return arrayParams;
 }
 console.log(evaluationDefaultArguments("test one"));
 // ["test one"]
@@ -55,10 +55,50 @@ console.log(evaluationDefaultArguments("test two"));
     ?We can use the values of the previous parameters (from left to right) as the default values of the next parameter
 */
 function areaOfSquareOrRectangle(sideOne, sideTwo = sideOne) {
-    return sideOne * sideTwo ;
+  return sideOne * sideTwo;
 }
 console.log(areaOfSquareOrRectangle(5));
 // 25
-console.log(areaOfSquareOrRectangle(5 , 2));
+console.log(areaOfSquareOrRectangle(5, 2));
 // 10
 
+/*
+    * arguments :
+        arguments is an Array-like object accessible inside functions that contains the values of the arguments passed to that function.
+        The value of the arguments object inside the function is the number of actual arguments that you pass to the function.
+*/
+
+function testArgumentsObject(
+  params1 = "default value P1",
+  params2 = "default value P2",
+  params3 = "default value P3"
+) {
+  console.log(arguments.length);
+  console.log(`*${params1}* , *${params2}* , *${params3}*`);
+}
+
+testArgumentsObject();
+// first log => 0
+// second log => *default value P1* , *default value P2* , *default value P3*
+
+testArgumentsObject("value one");
+// first log => 1
+// second log => *value one* , *default value P2* , *default value P3*
+
+testArgumentsObject("value one", "value two");
+// first log => 2
+// second log => *value one* , *value two* , *default value P3*
+
+/*
+    In the default parameters, the parameters with the default value should be written after the parameters without the default value. 
+    Because Parameters are still set left-to-right, overwriting default parameters even if there are later parameters without defaults
+*/
+
+function testParameter(params1 = "one", params2) {
+  return `${params1} , ${params2}`;
+}
+console.log(testParameter());
+// one , undefined
+
+console.log(testParameter("test"));
+// test , undefined
