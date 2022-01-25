5ebbac90d9b7490eaf6ec293ed2437acd0861cdd
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-01-25 18:39:14 +0330

    Codewars kata (Grasshopper - Summation)

--- Folder_8_(2022-01-25)/homework(codewars).js
+++ Folder_8_(2022-01-25)/homework(codewars).js
@@ -47,3 +47,26 @@ function removeEveryOther(arr) {
   });
   return arrResult;
 }
+
+/*
+ !# 4
+ ? Grasshopper - Summation
+ URL: https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/javascript
+
+ Summation
+ Write a program that finds the summation of every number from 1 to num. 
+ The number will always be a positive integer greater than 0.
+
+ For example:
+ summation(2) -> 3
+ 1 + 2
+ summation(8) -> 36
+ 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+*/
+const summation = (num) => {
+  let sum = 0;
+  while (num >= 0) {
+    sum += num--;
+  }
+  return sum;
+};
