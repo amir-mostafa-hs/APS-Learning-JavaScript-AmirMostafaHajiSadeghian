8aace3cd28aaf172be798d59767833740f06fd3d
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-01-26 00:34:47 +0330

    Codewars kata (Regular Ball Super Ball)

--- Folder_8_(2022-01-25)/homework(codewars).js
+++ Folder_8_(2022-01-25)/homework(codewars).js
@@ -118,3 +118,20 @@ function gooseFilter (birds) {
   });
 };
 
+/*
+ !# 7
+ ? Regular Ball Super Ball
+ URL: https://www.codewars.com/kata/53f0f358b9cb376eca001079/train/javascript
+
+ Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+ If no arguments are given, ball objects should instantiate with a "ball type" of "regular".
+
+ For example:
+ ball1 = new Ball();
+ ball2 = new Ball("super");
+ ball1.ballType     //=> "regular"
+ ball2.ballType     //=> "super"
+*/
+const Ball = function (ballType = "regular") {
+  this.ballType = ballType;
+};
