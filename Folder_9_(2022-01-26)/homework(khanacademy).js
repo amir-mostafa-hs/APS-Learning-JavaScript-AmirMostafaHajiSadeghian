914a96292d878cc1c35704127d992ec864a24c1d
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-01-26 11:25:01 +0330

    create folder 9

---
+++ Folder_9_(2022-01-26)/homework(khanacademy).js
@@ -0,0 +1,19 @@
+/*
+ !# JS and the DOM :
+
+ * script tag:
+    We use (tag script) to access JavaScript code in HTML file.
+    We write (script tag) in the end line (body) to execute other HTML tags for create the 
+    document and elements so that we can then access the document elements with JavaScript code.
+ 
+ * DOM (Document Object Model): 
+    The browser converts web pages (HTML and CSS) into a DOM. DOM is a tool provided by the browser to manage, 
+    edit or create web pages. The DOM looks like a JavaScript object. The DOM contains all the HTML tags and CSS styles we need.
+
+    We use the global variable document to access the DOM in JavaScript.
+    After writing the document, we can use the methods in it to access HTML elements and CSS styles.
+    The DOM is not our main web page and is just an emulator of it. 
+    The browser monitors the DOM and applies the changes to the homepage as soon as it changes.
+
+*/
+
