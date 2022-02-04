0b156650b897469d9a145b5232a4d12dbd59c769
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-02-04 16:17:02 +0330

    editing all file

--- Folder_11_(2022-02-04)/Reviewing-JS2-3-session.js
+++ Folder_11_(2022-02-04)/Reviewing-JS2-3-session.js
@@ -1,66 +1,19 @@
-document.body.style.margin = "0";
-document.body.style.width = "100vw";
-document.body.style.height = "100vh";
-document.body.style.display = "grid";
-document.body.style.gridTemplateColumns = "2fr 1fr";
-document.body.style.background = "linear-gradient(to right, #232526, #414345)";
+/*
+  *ravesh haye mokhtalefe estefade az Event:
+      1- darone html : baestefade az name event darone attribute haye yek element ke mamolan ba (on)
+         shro mishavand mesle => onclick , ondblclick , onmouseover , ...
+         { <p onclick="function for event">click</p> }
+        
+      2- in JavaScript by select element and add event name method. almost like the case above.
+         { const p = document.querySelector("p");
+           p.onclick = function(){} for event; }
+        
+      3- in JavaScript by select element and add addEventListener method.
+         in this add event, event names do not have the word (on) at the beginning.
+         { const p = document.querySelector("p");
+           p.addEventListener("event name for example => click" , function(){} for event) }
+*/
 
-const getShowEvent = document.getElementById("showEvent");
-getShowEvent.style.background = "linear-gradient(60deg, #ff6e7f, #bfe9ff)";
-getShowEvent.style.display = "flex";
-getShowEvent.style.justifyContent = "center";
-getShowEvent.style.alignItems = "center";
-getShowEvent.style.margin = "20px";
-getShowEvent.style.padding = "10px";
-getShowEvent.style.borderRadius = "10px";
+document.body.addEventListener;
 
-const getAsideEvent = document.getElementById("event");
-getAsideEvent.style.backgroundColor = "rgb(50,50,50,0.4)";
-getAsideEvent.style.padding = "10px";
-getAsideEvent.style.borderRadius = "5px";
-getAsideEvent.style.fontSize = "1.5rem";
-
-const getBtnReset = document.getElementById("reset");
-getBtnReset.style.alignSelf = "flex-end";
-getBtnReset.style.position = "absolute";
-const btnReset = () => (getAsideEvent.textContent = "");
-
-const getContent = document.getElementById("content");
-getContent.style.background =
-  "linear-gradient(45deg, rgba(174,238,227,1) 0%, rgba(230,191,181,1) 50%, rgba(188,148,233,1) 100%)";
-getContent.style.borderRadius = "10px";
-getContent.style.margin = "20px";
-getContent.style.padding = "10px";
-getContent.style.display = "grid";
-getContent.style.justifyItems = "center";
-getContent.style.alignContent = "space-evenly";
-getContent.style.gridTemplateColumns = " repeat(auto-fit, minmax(150px, 1fr))";
-
-function createAsideElemForTest(text) {
-  const aside = document.createElement("aside");
-  const randomColor = () => Math.floor(Math.random() * 256);
-  aside.style.backgroundColor = `rgb(${randomColor()},${randomColor()},${randomColor()})`;
-  aside.style.width = "100px";
-  aside.style.height = "100px";
-  aside.style.border = `solid rgb(${randomColor()},${randomColor()},${randomColor()})`;
-  aside.style.borderRadius = "20px";
-  aside.textContent = text;
-  aside.style.fontSize = "1.2rem";
-  aside.style.display = "flex";
-  aside.style.justifyContent = "center";
-  aside.style.alignItems = "center";
-  aside.style.color = "white";
-  return aside;
-}
-
-const asideForClickEvt = createAsideElemForTest("Click Event");
-asideForClickEvt.onclick = function () {
-  getAsideEvent.innerText += `You have now clicked \n`;
-};
-getContent.appendChild(asideForClickEvt);
-
-const asideForDragsEvt = createAsideElemForTest("Drags Event");
-asideForDragsEvt.addEventListener("drag",function(){
-    
-})
-getContent.appendChild(asideForDragsEvt);
+// function
