70ed76d9fada167dae6046fa61e6e3d6378a31f0
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-02-05 08:13:49 +0330

    Event description added

--- Folder_11_(2022-02-04)/Reviewing-JS2-3-session.js
+++ Folder_11_(2022-02-04)/Reviewing-JS2-3-session.js
@@ -12,8 +12,25 @@
          in this add event, event names do not have the word (on) at the beginning.
          { const p = document.querySelector("p");
            p.addEventListener("event name for example => click" , function(){} for event) }
+
+        ? NOTE: addEventListener raveshi hast ke baraye add kardan event tosye mishavad.
+              ? baraye bishtar event target ha kar mikonad na fa ghat HTML ya SVG elements.
+              
+              ? useCapture: It is an optional parameter used to control event propagation. 
+              ? A boolean value is passed where “true” denotes capturing phase and “false” denotes the bubbling phase.
+              
+              ? JavaScript provides an event handler in the form of the addEventListener() method. 
+              ? This handler can be attached to a specific HTML element you wish to monitor events for, 
+              ? and the element can have more than one handler attached.
+
+              Passing Event as a Parameter : Sometimes we may want to know more information about the event, 
+                      such as what element was clicked. In this situation, we need to pass in an event parameter to our function .
+
+                      This parameter is an object which contains various information about the event such as the target id.
+
+                      
 */
 
-document.body.addEventListener;
+// document.body.addEventListener;
 
 // function
