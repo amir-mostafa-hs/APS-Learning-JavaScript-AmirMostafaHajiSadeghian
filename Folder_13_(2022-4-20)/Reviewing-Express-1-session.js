704c2670fb7429e26731c24666bb1106fd5f31ce
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-04-20 16:28:49 +0430

    What is API

--- Folder_13_(2022-4-20)/Reviewing-Express-1-session.js
+++ Folder_13_(2022-4-20)/Reviewing-Express-1-session.js
@@ -1,4 +1,22 @@
 /*
+    !What is API:  (Application Programming Interface)
+        API is the acronym for Application Programming Interface,
+         which is a software intermediary that allows two applications to talk to each other. 
+        Each time you use an app like Facebook, send an instant message, or check the weather on your phone, you’re using an API.
+
+        When you use an application on your mobile phone, the application connects to the Internet and sends data to a server. 
+        The server then retrieves that data, interprets it, performs the necessary actions and sends it back to your phone. 
+        The application then interprets that data and presents you with the information you wanted in a readable way. 
+        This is what an API is - all of this happens via API.
+
+            #To explain this better, let us take a familiar example:
+                    Imagine you’re sitting at a table in a restaurant with a menu of choices to order from. 
+                    The kitchen is the part of the “system” that will prepare your order. 
+                    What is missing is the critical link to communicate your order to the kitchen and
+                     deliver your food back to your table. That’s where the waiter or API comes in. 
+                    The waiter is the messenger – or API – that takes your request or order and tells
+                     the kitchen – the system – what to do. Then the waiter delivers the response back to you; in this case, it is the food.
+
     !What is express js in node js:
         Express.js, or simply Express, is a back end web application framework for Node.js,
          released as free and open-source software under the MIT License. 
@@ -10,5 +28,4 @@
         Express is the back-end component of popular development stacks like the MEAN, MERN or MEVN stack,
          together with the MongoDB database software and a JavaScript front-end framework or library.
 
-        
 */
