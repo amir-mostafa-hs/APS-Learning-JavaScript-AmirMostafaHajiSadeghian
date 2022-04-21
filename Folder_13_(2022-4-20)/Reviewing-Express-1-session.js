c0e430ff35579c87b7331990df101d31e70f9cfe
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-04-21 06:42:44 +0430

    What is request(req) in Express

--- Folder_13_(2022-4-20)/Reviewing-Express-1-session.js
+++ Folder_13_(2022-4-20)/Reviewing-Express-1-session.js
@@ -81,5 +81,14 @@
             ?   console.log("run use function");
             ? });
 
+    * What is request(req) in Express:
+            The req object represents the HTTP request and has properties for the request query string,
+             parameters, body, HTTP headers, and so on. 
+            In this documentation and by convention, the object is always referred to as req
+             (and the HTTP response is res) but its actual name is determined by the parameters 
+             to the callback function in which you’re working.
 
+            ? app.use((req,?)) => {
+            ?   console.log("run use function");
+            ? }); 
 */
